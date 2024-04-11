3a30ce41aadf4b530f878d9d51423a3a0c9a2699
neko <nekogamecn@gmail.com>
2024-04-11 17:52:07 +0800

    fix: vite base 2

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tools/",
+  base: "/tools",
   plugins: [
     vue(),
     AutoImport({ resolvers: [ElementPlusResolver()] }),

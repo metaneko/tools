446b0ef07d4bac06f8c011c1ca55cda24e257535
neko <nekogamecn@gmail.com>
2024-04-17 15:18:43 +0800

    fix: delete vite base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tools",
   plugins: [
     vue(),
     AutoImport({ resolvers: [ElementPlusResolver()] }),

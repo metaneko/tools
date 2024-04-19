1cf2ed554d678365bee080f295ab15c1aeb41b60
neko <nekogamecn@gmail.com>
2024-04-19 11:50:43 +0800

    fix: use h5 router

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import { routes } from './routes';
 
 const pinia = createPinia();
-const router = createRouter({ history: createWebHashHistory(), routes });
+const router = createRouter({ history: createWebHistory(), routes });
 const app = createApp(App);
 
 app.use(pinia);
